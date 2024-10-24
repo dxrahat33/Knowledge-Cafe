a1e643be2897875a3499c3be0306bb4b67db308f
dxrahat33 <dxrahat33@gmail.com>
2024-10-24 20:48:49 +0600

    project initial ready with tailwindCss

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-3xl bg-red-500 p-10 text-center text-white'>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
